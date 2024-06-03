e9c62867be008ec2abdc851beb7f125a5d340802
react-guru007 <passionguy211@gmail.com>
2024-06-02 22:22:08 -0500

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: (origin, callback) => {
-      const whitelist = [
-        'http://localhost:3000',
-        'https://points-ui-one.vercel.app',
-      ];
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(3000);
